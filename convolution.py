60d870b33793c2449d6c07b025e89a5d939bed39
Alan <alanrabelo13@gmail.com>
2019-09-17 12:21:18 -0300

    Other filters

--- convolution.py
+++ convolution.py
@@ -2,95 +2,102 @@ from PIL import Image
 import numpy as np
 import random
 import math
+import os
 
 class Convolution:
 
     IMAGE_MODE = 'L'
 
+    def __init__(self, kernel_size):
+        self.kernel_size = kernel_size
+
     def load_image(self, filename):
-        img = Image.open(filename).convert(self.IMAGE_MODE)
+        self.filename = filename
+        img = Image.open('Sample Images/' + filename + '.jpg').convert(self.IMAGE_MODE)
         img.load()
         data = np.asarray(img)
-
-        data = np.pad(data, pad_width=1, mode='reflect')
         return data
 
+    def save_image(self, filename, image):
+
+        path = 'Outputs/' + self.filename
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        image_cropped = image[self.kernel_size:-self.kernel_size, self.kernel_size:-self.kernel_size]
+
+        new_image = Image.fromarray(image_cropped, mode=self.IMAGE_MODE)
+        new_image.save('Outputs/%s/%s.jpg' % (self.filename, filename))
+        return image
+
+    def scale(self, X, x_min=0, x_max=255):
+        nom = (X - X.min()) * (x_max - x_min)
+        denom = X.max() - X.min()
+        denom = denom + (denom is 0)
+        return x_min + np.array(nom / denom)
+
     def salt_and_pepper(self, image, probability_threshold=0.95):
-        new_image = np.array(image)
+        new_image = np.pad(image, pad_width=self.kernel_size, mode='edge')
         height = len(image)
         width = len(image[0])
 
         for row_index in range(height):
             for col_index in range(width):
-                # for dimension_index in range(3):
-
                     rand = random.uniform(0, 1)
-
                     if rand > probability_threshold:
                         black_or_white = 0 if bool(random.getrandbits(1)) == 0 else 255
-                        # new_image[row_index, col_index, dimension_index] = black_or_white
                         new_image[row_index, col_index] = black_or_white
 
-        s_n_p = Image.fromarray(new_image, mode=self.IMAGE_MODE)
-        s_n_p.save('Outputs/salt_and_pepper.jpg')
-        return new_image
+        return self.save_image('salt_and_pepper', new_image)
 
-    def filter_average(self, image):
-
-        matrix = [
-            [1, 1, 1],
-            [1, 1, 1],
-            [1, 1, 1]
-        ]
-        kernel_size = len(matrix)
-
+    def filter_average(self, image, kernel):
+        kernel_size = len(kernel)
         new_image = np.array(image)
         height = len(image)
         width = len(image[0])
-        kernel_center = math.floor(len(matrix) / 2)
+        kernel_center = math.floor(len(kernel) / 2)
 
-        # new_image = np.insert(new_image, 0, new_image[:, :kernel_center, :], axis=1)
-        # new_image = np.insert(new_image, 0, new_image[:, -1:, :], axis=1)
-
-        for row_index in range(height-kernel_size):
-            for col_index in range(width-kernel_size):
-                # for dimension_index in range(3):
+        for row_index in range(height-kernel_size+1):
+            for col_index in range(width-kernel_size+1):
                     image_part = image[row_index:row_index+kernel_size, col_index:col_index+kernel_size]
-                    # image_part = image[row_index:row_index+kernel_size, col_index:col_index+kernel_size, dimension_index]
-                    result = matrix * image_part
+                    result = kernel * image_part
                     result_average = np.average(result)
                     new_image[row_index + kernel_center, col_index + kernel_center] = result_average
-                    # new_image[row_index + kernel_center, col_index + kernel_center, dimension_index] = result_average
 
-        s_n_p = Image.fromarray(new_image, mode=self.IMAGE_MODE)
-        s_n_p.show()
-        s_n_p.save('Outputs/average.jpg')
+        return self.save_image('average', new_image)
 
-    def filter_median(self, image):
+    def filter_median(self, image, kernel):
 
-        matrix = [
-            [1, 1, 1],
-            [1, 1, 1],
-            [1, 1, 1]
-        ]
-        kernel_size = len(matrix)
+        kernel_size = len(kernel)
 
-        new_image = np.array(image)
+        new_image = np.array(image, dtype='uint8')
         height = len(image)
         width = len(image[0])
-        kernel_center = math.floor(len(matrix) / 2)
+        kernel_center = math.floor(len(kernel) / 2)
 
-        for row_index in range(height-kernel_size):
-            for col_index in range(width-kernel_size):
-                # for dimension_index in range(3):
-
+        for row_index in range(height-kernel_size+1):
+            for col_index in range(width-kernel_size+1):
                     image_part = image[row_index:row_index+kernel_size, col_index:col_index+kernel_size]
-                    # image_part = image[row_index:row_index+kernel_size, col_index:col_index+kernel_size, dimension_index]
-                    result = matrix * image_part
+                    result = kernel * image_part
                     result_average = np.median(result)
-                    # new_image[row_index + kernel_center, col_index + kernel_center, dimension_index] = result_average
                     new_image[row_index + kernel_center, col_index + kernel_center] = result_average
 
-        s_n_p = Image.fromarray(new_image, mode=self.IMAGE_MODE)
-        s_n_p.show()
-        s_n_p.save('Outputs/median.jpg')
+        return self.save_image('median', new_image)
+
+    def convolve(self, image, kernel, filename):
+
+        kernel_size = len(kernel)
+
+        new_image = np.array(image, dtype='uint8')
+        height = len(image)
+        width = len(image[0])
+        kernel_center = math.floor(len(kernel) / 2)
+
+        for row_index in range(height-kernel_size+1):
+            for col_index in range(width-kernel_size+1):
+                    image_part = image[row_index:row_index+kernel_size, col_index:col_index+kernel_size]
+                    result = kernel * image_part
+                    result_average = np.average(result)
+                    new_image[row_index + kernel_center, col_index + kernel_center] = result_average
+
+        return self.save_image(filename, new_image)
--- main.py
+++ main.py
@@ -1,7 +1,64 @@
 from convolution import Convolution
+import numpy as np
 
-convolution = Convolution()
-image = convolution.load_image('Sample Images/lena_low.jpg')
-image = convolution.salt_and_pepper(image)
-convolution.filter_average(image)
-# convolution.filter_median(image)
+kernel = np.array([
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1]
+          ])
+
+sobel_H = np.array([
+    [-1, 0, 1],
+    [-2, 0, 2],
+    [-1, 0, 1]
+          ])
+
+sobel_V = np.array([
+    [-1, -2, -1],
+    [0, 0, 0],
+    [1, 2, 1]
+          ])
+
+laplacian_1 = np.array([
+    [0, -1, 0],
+    [-1, 4, -1],
+    [0, -1, 0]
+          ])
+
+laplacian_2 = np.array([
+    [-1, -1, -1],
+    [-1, 8, -1],
+    [-1, -1, -1]
+          ])
+
+prewitt_1 = np.array([
+    [-1, 0, 1],
+    [-1, 0, 1],
+    [-1, 0, 1]
+          ])
+
+prewitt_2 = np.array([
+    [-1, -1, -1],
+    [0, 0, 0],
+    [1, 1, 1]
+          ])
+
+images = ['lena_original']
+# images = ['lena_original', 'zebra', 'manhattan_medium']
+
+for image in images:
+    convolution = Convolution(kernel_size=len(kernel))
+    image = convolution.load_image('%s' % image)
+    image = convolution.filter_average(image, kernel)
+    image = convolution.filter_average(image, kernel)
+
+    convolution.convolve(image, sobel_H, 'sobel_H')
+    convolution.convolve(image, sobel_V, 'sobel_V')
+    convolution.convolve(image, laplacian_1, 'laplacian1')
+    convolution.convolve(image, laplacian_2, 'laplacian2')
+    convolution.convolve(image, prewitt_1, 'prewitt_1')
+    convolution.convolve(image, prewitt_2, 'prewitt_2')
+    image = convolution.salt_and_pepper(image)
+    convolution.filter_average(image, kernel)
+    convolution.filter_median(image, kernel)
+
